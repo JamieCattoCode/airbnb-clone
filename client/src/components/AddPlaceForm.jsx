78b05f7f9765a39b638da87b230b020b005cbe3d
JamieCattoCode <jamietcatto@gmail.com>
2023-06-21 07:03:57 +0200

    Perks now working with state

--- client/src/components/AddPlaceForm.jsx
+++ client/src/components/AddPlaceForm.jsx
@@ -72,11 +72,11 @@ function AddPlaceForm() {
         </div>
         <div className="mt-2 grid gap-2 grid-cols-3 lg:grid-cols-6 md:grid-cols-4">
           {addedPhotos.length > 0 && addedPhotos.map((link) => (
-            <div>
-              <img className="rounded-2xl" src={`http://localhost:4000/uploads/${link}`} alt="destination" />
+            <div className="h-32 flex">
+              <img className="rounded-2xl w-full object-cover" src={`http://localhost:4000/uploads/${link}`} alt="destination" />
             </div>
           ))}
-          <label className="cursor-pointer border bg-transparent rounded-2xl p-8 text-2xl text-gray-600 flex justify-center items-center gap-1">
+          <label className="h-32 cursor-pointer border bg-transparent rounded-2xl p-8 text-2xl text-gray-600 flex justify-center items-center gap-1">
             <input type="file" className="hidden" onChange={uploadPhoto} />
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
